038922b15ab3ce887fea91d799171438826684c6
Umut Hakan Demir <umuthakandemir724@gmail.com>
2023-11-30 23:43:09 +0300

    Diziler

---
+++ Ders4 Diziler/Ders4 Diziler/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Ders4_Diziler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblelemanlar = new Label();
+            SuspendLayout();
+            // 
+            // lblelemanlar
+            // 
+            lblelemanlar.AutoSize = true;
+            lblelemanlar.Location = new Point(156, 18);
+            lblelemanlar.Name = "lblelemanlar";
+            lblelemanlar.Size = new Size(52, 21);
+            lblelemanlar.TabIndex = 0;
+            lblelemanlar.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(369, 338);
+            Controls.Add(lblelemanlar);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblelemanlar;
+    }
+}
---
+++ Ders4 Diziler/Ders4 Diziler/Form1.cs
@@ -0,0 +1,23 @@
+﻿namespace Ders4_Diziler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] sayilar = new int[4];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // lblelemanlar.Text = sayilar[1].ToString();
+            sayilar[3] = 2;
+            sayilar[2] = 5;
+            sayilar[1] = 6;
+            sayilar[0] = 78;
+            //lblelemanlar.Text=  sayilar[sayilar.Length - 1].ToString();
+            sayilar[sayilar.Length - 1] = 45;
+            lblelemanlar.Text = sayilar[sayilar.Length - 1].ToString();
+            //lblelemanlar.Text = "1.sayı: "+sayilar[0].ToString()+" 2.sayı: "+sayilar[3].ToString();
+        }
+    }
+}
